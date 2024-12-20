cb178abfbcc065acb360296c23d6d025c54b5fa1
foufouboy <cissaclement@gmail.com>
2024-12-20 09:51:44 +0100

    finished membership adding and all backend/logic stuff

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,6 +1,11 @@
 const userDropdown = document.querySelector(".user-nav .drop-down");
 const userNav = document.querySelector(".user-nav");
 
+const deleteButtons = document.querySelectorAll("button.delete-post");
+const membershipLink = document.querySelector("a.join-membership");
+
+// Header dropdown
+
 if (userDropdown && userNav) {
     document.body.addEventListener("click", (e) => {
 
@@ -25,3 +30,64 @@ if (userDropdown && userNav) {
     })
 }
 
+// Delete post buttons
+
+if (deleteButtons.length) {
+    const deletePost = (e) => {
+        const postId = e.target.getAttribute("data-post-id");
+
+        fetch(`/delete-post/${postId}`, {
+            method: "POST",
+            redirect: "follow",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({id: postId}),
+        })
+        .then(response => {
+            if (response.redirected) {
+                window.location.href = response.url;
+            }
+        })
+        .catch(err => {
+            console.error(err);
+        });
+    }
+
+    deleteButtons.forEach(button => {
+        console.log(button);
+        button.addEventListener("click", (e) => deletePost(e));
+    });
+}
+
+// Membership joining
+
+if (membershipLink) {
+    membershipLink.addEventListener("click", e => {
+        e.preventDefault();
+
+        const secret = prompt(`Please enter the super secret password : "dogs are much better than cats".`);
+
+        if (secret === "dogs are much better than cats") {
+
+            fetch(`/join`, {
+                method: "POST",
+                redirect: "follow",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+            })
+            .then(response => {
+                if (response.redirected) {
+                    window.location.href = response.url;
+                }
+            })
+            .catch(err => {
+                console.error(err);
+            });
+
+        } else {
+            alert("Wrong secret code! (Admit the truth)")
+        }
+    });
+}
