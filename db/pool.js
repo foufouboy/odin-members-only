3ca0118e172541a870fc7497cef2902a646f38da
foufouboy <cissaclement@gmail.com>
2024-12-21 16:54:43 +0100

    changed the db url

--- db/pool.js
+++ db/pool.js
@@ -2,5 +2,5 @@ const pg = require("pg");
 require("dotenv").config();
 
 module.exports = new pg.Pool({
-    connectionString: process.env.DB_URL_PROD,
+    connectionString: process.env.DB_URL,
 });
